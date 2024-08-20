f5f6248a0340d7e02246e16af64e847f9b7cfedf
madyanes <madyanes.dev@gmail.com>
2024-08-20 07:03:53 +0700

    Just to show that CSS class '.nav' is unique
    Unique for each component.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom'
 import PageNav from '../components/PageNav'
+import AppNav from '../components/AppNav'
 
 function Homepage() {
   return (
     <>
       <PageNav />
+      <AppNav />
 
       <h1>WorldWise</h1>
+
+      <Link to='/app'>Go to the app</Link>
     </>
   )
 }
