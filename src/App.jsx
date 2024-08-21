9384ed498510622b53e8974e02f52b79c4c6a102
madyanes <madyanes.dev@gmail.com>
2024-08-21 08:28:32 +0700

    Retrieve cities from fake API

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,33 @@ import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from './pages/AppLayout'
+import CityList from './components/CityList'
 import './index.css'
+import { useEffect, useState } from 'react'
+
+const BASE_URL = 'http://localhost:8000'
 
 function App() {
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
+      } catch {
+        alert('There was an error loading data...')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchCities()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +40,14 @@ function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path='cities' element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path='cities'
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path='countries' element={<p>List of countries</p>} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+
+import CityItem from './CityItem'
+import styles from './CityList.module.css'
+import Spinner from './Spinner'
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  )
+}
+
+export default CityList
