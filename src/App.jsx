f6b79f0dc3f1578a57aaa1f57c33fad5dc185ce6
madyanes <madyanes.dev@gmail.com>
2024-08-21 21:04:44 +0700

    Navigation buttons on the form

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import './index.css'
 import { useEffect, useState } from 'react'
 import CountryList from './components/CountryList'
 import City from './components/City'
+import Form from './components/Form'
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -55,7 +56,7 @@ function App() {
             path='countries'
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path='form' element={<p>Form</p>} />
+          <Route path='form' element={<Form />} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
