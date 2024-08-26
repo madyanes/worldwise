b4b8abb7f97462019fc95663ecfbb1e22856862a
madyanes <madyanes.dev@gmail.com>
2024-08-26 14:55:10 +0700

    Set states of cityName and country from the map
    From the clicked event of the map.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useUrlPosition } from '../hooks/useUrlPosition'
 
 import styles from './Form.module.css'
 import Button from './Button'
@@ -14,11 +15,34 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints)
 }
 
+const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client'
+
 function Form() {
+  const [lat, lng] = useUrlPosition()
   const [cityName, setCityName] = useState('')
   const [country, setCountry] = useState('')
   const [date, setDate] = useState(new Date())
   const [notes, setNotes] = useState('')
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false)
+
+  useEffect(() => {
+    async function fetchCityData() {
+      try {
+        setIsLoadingGeocoding(true)
+        const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`)
+        const data = await res.json()
+        console.log('city data', data)
+        setCityName(data.city || data.locality || '')
+        setCountry(data.countryName)
+      } catch (err) {
+        //
+      } finally {
+        setIsLoadingGeocoding(false)
+      }
+    }
+
+    fetchCityData()
+  }, [lat, lng])
 
   return (
     <form className={styles.form}>
