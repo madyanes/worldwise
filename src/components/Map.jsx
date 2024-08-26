1b32c931ced0410b7ec9b61a0dc695712530c5de
madyanes <madyanes.dev@gmail.com>
2024-08-26 14:00:54 +0700

    Hide location button if no geolocation position
    Hide the 'get current location' button if there's no geolocation
    position.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -37,9 +37,12 @@ function Map() {
 
   return (
     <div className={styles.mapContainer}>
-      <Button type='position' onClick={getPosition}>
-        {isLoadingPosition ? 'Loading...' : 'Use your position'}
-      </Button>
+      {!geolocationPosition && (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
+        </Button>
+      )}
+
       <MapContainer
         center={mapPosition}
         zoom={6}
