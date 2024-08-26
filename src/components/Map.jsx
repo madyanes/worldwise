c91b98ff1ac891284e6a1ba0bc19b4ea02f83588
madyanes <madyanes.dev@gmail.com>
2024-08-26 13:52:10 +0700

    Get the current geolocation of the user
    Using custom hook.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,13 +9,20 @@ import {
   useMapEvents,
 } from 'react-leaflet'
 import { useCities } from '../contexts/CitiesContext'
+import { useGeolocation } from '../hooks/useGeolocation'
+import Button from './Button'
 import styles from './Map.module.css'
 
 function Map() {
   const { cities } = useCities()
   const [mapPosition, setMapPosition] = useState([40, 0]) // expects lat and lng
-
   const [searchParams] = useSearchParams()
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation()
+
   const mapLat = searchParams.get('lat')
   const mapLng = searchParams.get('lng')
 
@@ -23,8 +30,16 @@ function Map() {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
   }, [mapLat, mapLng])
 
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+  }, [geolocationPosition])
+
   return (
     <div className={styles.mapContainer}>
+      <Button type='position' onClick={getPosition}>
+        {isLoadingPosition ? 'Loading...' : 'Use your position'}
+      </Button>
       <MapContainer
         center={mapPosition}
         zoom={6}
