f840dac8218bd0babbfe6342fb6b699436b9cbe8
madyanes <madyanes.dev@gmail.com>
2024-08-28 20:54:41 +0700

    Display logged in user from context

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,14 +1,8 @@
+import { useAuth } from '../contexts/FakeAuthContext'
 import styles from './User.module.css'
 
-const FAKE_USER = {
-  name: 'Jack',
-  email: 'jack@example.com',
-  password: 'qwerty',
-  avatar: 'https://i.pravatar.cc/100?u=zz',
-}
-
 function User() {
-  const user = FAKE_USER
+  const { user } = useAuth()
 
   function handleClick() {}
 
@@ -29,6 +23,6 @@ CHALLENGE
 1) Add `AuthProvider` to `App.jsx` ✅
 2) In the `Login.jsx` page, call `login()` from context ✅
 3) Inside an effect, check whether `isAuthenticated === true`. If so, programatically navigate to `/app` ✅
-4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js`
+4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js` ✅
 5) Handle logout button by calling `logout()` and navigating back to `/`
 */
